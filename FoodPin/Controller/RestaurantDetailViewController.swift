3fadf963c16f4c4144e9f82af8f14326a6e9b253
zachang890 <zachang890@gmail.com>
2020-08-23 16:13:25 -0700

    Add details

--- FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -10,33 +10,46 @@ import UIKit
 
 class RestaurantDetailViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        restaurantImageView.image = UIImage(named: restaurant.image)
-        nameLabelView.text = restaurant.name
-        typeLabelView.text = restaurant.type
-        locationLabelView.text = restaurant.location
-        
-        navigationItem.largeTitleDisplayMode = .never
-        // Do any additional setup after loading the view.
-    }
-    
-    @IBOutlet var restaurantImageView: UIImageView!
-    @IBOutlet var nameLabelView: UILabel!
-    @IBOutlet var typeLabelView: UILabel!
-    @IBOutlet var locationLabelView: UILabel!
     
     var restaurant = Restaurant()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
+
+//class RestaurantDetailViewController: UIViewController {
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        restaurantImageView.image = UIImage(named: restaurant.image)
+//        nameLabelView.text = restaurant.name
+//        typeLabelView.text = restaurant.type
+//        locationLabelView.text = restaurant.location
+//
+//        navigationItem.largeTitleDisplayMode = .never
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    @IBOutlet var restaurantImageView: UIImageView!
+//    @IBOutlet var nameLabelView: UILabel!
+//    @IBOutlet var typeLabelView: UILabel!
+//    @IBOutlet var locationLabelView: UILabel!
+//
+//    var restaurant = Restaurant()
+//
+//
+//    /*
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
+//
+//}
