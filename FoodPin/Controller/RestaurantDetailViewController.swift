a5943baa77a649f617055df2fd5612e040f54466
zachang890 <zachang890@gmail.com>
2020-08-27 12:40:02 -0700

    Final

--- FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -50,6 +50,7 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
             case 4:
                 let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailMapCell.self), for: indexPath) as! RestaurantDetailMapCell
                 cell.selectionStyle = .none
+                cell.configure(location: restaurant.location)
                 return cell
             default:
                 fatalError("Failed to instantiate the table view cell for detail view controller") }
